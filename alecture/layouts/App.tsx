f488cb82e55e86aedf79690c01f39c21e495923e
jeongho218 <82637024+jeongho218@users.noreply.github.com>
2024-01-01 21:17:29 +0900

    코드 스플리팅과 이모션 1. loadable component 설치 $ npm i @loadable/component $ npm i -D @types/loadable__component
    2. /layouts/App.tsx 수정
    페이지 불러오는 방식을 직접 import에서
    loadble() 함수 사용 방식으로 변경
    
    3.  이모션 설치
    $ npm i @emotion/react @emotion/styled
    
    4. /pages/SignUp/index.tsx, styles.tsx 내용 작성
    styles.tsx에서 커스텀 태그를 작성하고,
    index.tsx에서 그 태그를 사용하여 html을 작성함

--- alecture/layouts/App.tsx
+++ alecture/layouts/App.tsx
@@ -1,7 +1,9 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
-import LogIn from "@pages/LogIn";
-import SignUp from "@pages/SignUp";
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import loadable from '@loadable/component';
+
+const SignUp = loadable(() => import('@pages/SignUp'));
+const LogIn = loadable(() => import('@pages/LogIn'));
 
 const App = () => {
   return (

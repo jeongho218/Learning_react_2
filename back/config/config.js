1cfed2f77f43be589732f9a01a24131940e78070
jeongho218 <82637024+jeongho218@users.noreply.github.com>
2024-01-01 10:36:09 +0900

    기본 세팅 프론트 작업을 진행하기 앞서 백엔드와 DB 연결 및 초기화 작업 .env, config.js 수정 DB, 테이블, 초기 데이터(seed) 생성 백엔드의 포트는 3095

--- back/config/config.js
+++ back/config/config.js
@@ -2,24 +2,24 @@ require('dotenv').config();
 
 module.exports = {
   "development": {
-    "username": "root",
-    "password": process.env.MYSQL_PASSWORD,
-    "database": "sleact",
-    "host": "127.0.0.1",
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_DATABASE,
+    "host": process.env.DB_HOST,
     "dialect": "mysql"
   },
   "test": {
-    "username": "root",
-    "password": process.env.MYSQL_PASSWORD,
-    "database": "sleact",
-    "host": "127.0.0.1",
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_DATABASE,
+    "host": process.env.DB_HOST,
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": process.env.MYSQL_PASSWORD,
-    "database": "sleact",
-    "host": "127.0.0.1",
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_DATABASE,
+    "host": process.env.DB_HOST,
     "dialect": "mysql"
   }
 }
